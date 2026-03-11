70ea47f8e5b9f8e400ced66895a666613017c88f
DiNerdyYawdie <62502925+DiNerdyYawdie@users.noreply.github.com>
2026-03-11 00:50:50 -0400

    Initial Commit

---
+++ Never Have I Ever/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Never Have I Ever
+//
+//  Created by Nerdy Yawdie on 3/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Never Have I Ever/Never_Have_I_EverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Never_Have_I_EverApp.swift
+//  Never Have I Ever
+//
+//  Created by Nerdy Yawdie on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Never_Have_I_EverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
